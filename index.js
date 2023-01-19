154503d10b67e50c7b2c24e53582dabef5ae75b3
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2023-01-19 16:44:01 +0100

    app.listen changed

--- index.js
+++ index.js
@@ -318,6 +318,11 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-app.listen(5500, () => {
-    console.log('Your app is listening on port 5500.');
-  });
+// app.listen(5500, () => {
+//     console.log('Your app is listening on port 5500.');
+//   });
+
+const port = process.env.PORT || 5500;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
+});
