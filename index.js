cf50492391ffb258061125a5cd74434cae42a90f
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2023-02-07 10:23:58 +0100

    adding back passport authentication for task 3.5

--- index.js
+++ index.js
@@ -43,8 +43,7 @@ app.get('/documentation', (req, res) => {
 });
 
 // get all movies
-app.get('/movies', 
-// passport.authenticate('jwt',{session:false}),
+app.get('/movies', passport.authenticate('jwt',{session:false}),
 (req,res) => {
     Movies.find()
     .then((movies) => {
